f8148e90f65b55769ec98adad8d834dba64021e1
LIMJIALIH <24004471@siswa.um.edu.my>
2024-11-28 15:45:03 +0800

    Lab 1 Question 3

---
+++ src/l1q3/L1Q3.java
@@ -0,0 +1,16 @@
+package l1q3;
+
+
+public class L1Q3 {
+    public static void main (String[]args) {
+        System.out.println("*********     ***          *            *");
+        System.out.println("*       *    *   *        ***          * *");
+        System.out.println("*       *   *     *      *****        *   *");
+        System.out.println("*       *   *     *        *         *     *");
+        System.out.println("*       *   *     *        *        *       *");
+        System.out.println("*       *   *     *        *         *     *");
+        System.out.println("*       *   *     *        *          *   *");
+        System.out.println("*       *    *   *         *           * *");
+        System.out.println("*********     ***          *            *");
+    }
+}
