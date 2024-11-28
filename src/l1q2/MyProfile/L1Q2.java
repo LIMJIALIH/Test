fa1c0be1ed902d5af1c88afbf73bdfce14e1cc13
LIMJIALIH <24004471@siswa.um.edu.my>
2024-11-28 15:34:23 +0800

    Lab 1 Question

---
+++ src/l1q2/MyProfile/L1Q2.java
@@ -0,0 +1,14 @@
+package l1q2.MyProfile;
+
+public class L1Q2 {
+
+    public static void main(String[] args) {
+        System.out.print("\nName:LIM JIA LIH");
+        System.out.print("\nMatric Number:24004471");
+        System.out.print("\nAddress:No30, Lorong Lima, Taman Seri Bintong Maju, 01000 Kangar Perlis");
+        System.out.print("\nEmail:limjl0130@gmail.com");
+        System.out.print("\nContact:019-4436683");
+        
+                }
+    
+}
